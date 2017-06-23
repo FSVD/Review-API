b1cb8651fd60d1a21d61af340a1ed1ab71439f6d
FSVD <fabio.schettino.01@gmail.com>
2017-06-23 17:40:11 +0200

    Connectors and models added to graphql architecture

---
+++ server/lib/app-1/review/review.connectors.js
@@ -0,0 +1,9 @@
+export function setTitle(customTitle) {
+  const title = 'The title review is: ' + customTitle;
+  return title;
+}
+
+export function setContent(customContent) {
+  const title = 'The review content is: ' + customContent;
+  return title;
+}
---
+++ server/lib/app-1/review/review.model.js
@@ -0,0 +1,2 @@
+// In this file we'll create models based on our ORM (in this cade Bookshelf models)
+// After we'll import this model into connector
