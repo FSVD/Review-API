a3265b5b9ed8e217a21b79da1c63034c3791d00f
FSVD <fabio.schettino.01@gmail.com>
2017-07-03 16:47:17 +0200

    Mutation updated

--- server/lib/GraphQL-api/user/user.connectors.js
+++ server/lib/GraphQL-api/user/user.connectors.js
@@ -6,15 +6,27 @@ export function getUserData(obj, args, context, info) {
 }
 
 export function generateFullName(obj, args, context, info) {
-  return `${obj.first_name} ${obj.last_name}`;
+  return UserModel.where('id', obj.id)
+    .fetch({ columns: ['first_name', 'last_name'] })
+    .then((result) => {
+      const parsedResult = JSON.parse(JSON.stringify(result));
+      const fullName = `${parsedResult.first_name} ${parsedResult.last_name}`;
+      return fullName;
+    });
 }
 
 export function firstNameToUpperCase(obj, args, context, info) {
-  return obj.firstName.toUpperCase();
+  return UserModel.where('id', obj.id)
+    .fetch({ columns: ['first_name'] })
+    .then((result) => {
+      const parsedResult = JSON.parse(JSON.stringify(result));
+      const firstNameUpperCased = `${parsedResult.first_name}`.toUpperCase();
+      return firstNameUpperCased;
+    });
 }
 
 export function addUser(obj, args, context, info) {
-  return UserModel.forge({
+  const newUser = {
     username: args.username || 'placeholderUsername',
     first_name: args.firstName,
     last_name: args.lastName,
@@ -25,12 +37,24 @@ export function addUser(obj, args, context, info) {
     user_account_status: args.userAccountStatus || 1,
     created_at: new Date(),
     updated_at: new Date(),
-  })
+  };
+  return UserModel.forge(newUser)
     .save()
     .then((result) => {
       const parsedResult = JSON.parse(JSON.stringify(result));
-      const newUserId = { id: parsedResult.id };
-      return getUserData(obj, newUserId, context, info);
+      const newInsertedUser = {
+        id: parsedResult.id,
+        username: parsedResult.username,
+        firstName: parsedResult.first_name,
+        lastName: parsedResult.last_name,
+        password: parsedResult.password,
+        email: parsedResult.email,
+        city: parsedResult.city,
+        newsletterAgree: parsedResult.newsletter_agree,
+        userAccountStatus: parsedResult.user_account_status,
+        reviews: [],
+      };
+      return newInsertedUser;
     })
     .catch((err) => {
       return Promise.reject({ error: err, message: err.message, propagatedBy: { module: 'user connector', function: 'addUser' } });
