8f0b3b508e8a542763cc657caf34518b3fa09fbc
FSVD <fabio.schettino.01@gmail.com>
2017-07-13 15:09:57 +0200

    cache update after mutatuion and optimistic UI implemented to deal with network latency

--- client/src/components/AddSubject.js
+++ client/src/components/AddSubject.js
@@ -7,39 +7,56 @@ import {
 import { subjectsListQuery } from './SubjecListWithData';
 
 const AddSubject = ({ mutate }) => {
-  const handleKeyUp = (evt) => {
-    if (evt.keyCode === 13) {
-      evt.persist();
-      console.log(evt.target.value);
-      mutate({
-        // Even if there is a subjectCategoryId in the form the value sended to server is always 1 because I don't know how to pass multiple fields values :)
-        variables: { subjectCategoryId:1, googlePlacesReference: evt.target.value },
-        refetchQueries: [{ query: subjectsListQuery }],
-      })
-      .then( res => {
-        evt.target.value = '';
-      });
-    }
-  }
+  const handleSubmit = (evt) => {
+    evt.persist();
+    //console.log(this.subjectCategoryId.value);
+    //console.log(this.googlePlacesReference.value);
+    mutate({
+      variables: { subjectCategoryId: this.subjectCategoryId.value, googlePlacesReference: this.googlePlacesReference.value },
+      // refetchQueries: [{ query: subjectsListQuery }], // Instead of refeching data, which means make a second roundtrip from client to server, we'll use update property exposed in mutate by Apollo CLient
+      optimisticResponse: {
+        addSubject: {
+          subjectCategoryId: this.subjectCategoryId.value,
+          googlePlacesReference: this.googlePlacesReference.value,
+          id: Math.round(Math.random() * -1000000),
+          __typename: 'Subject',
+        }
+      },
+      update: (cache, { data: { addSubject }}) => {
+        // console.log(addSubject);
+        const data = cache.readQuery({ query: subjectsListQuery });
+        // console.log(data);
+        data.subjects.push(addSubject);
+        // console.log(data);
+        cache.writeQuery({ query: subjectsListQuery, data });
+      },
+    })
+    .then(res => {
+      this.subjectCategoryId.value = '';
+      this.googlePlacesReference.value = '';
+    });
+  };
+
+  const handleSubjectCategoryIdChange = (evt) => {
+    //this.setState({ subjectCategoryId: evt.target.value });
+    //console.log(evt.target.value);
+  };
+
+  const handleGooglePlacesReferenceChange = (evt) => {
+    //this.setState({ googlePlacesReference: evt.target.value });
+    //console.log(evt.target.value);
+  };
 
   return (
     <div>
       <form>
-        <input
-          key="subjectCategoryId"
-          type="number"
-          placeholder="Category ID"
-        />
-        <input
-          key="googlePlacesReference"
-          type="text"
-          placeholder="New subject"
-          onKeyUp={handleKeyUp}
-        />
+        <input ref={input => this.subjectCategoryId = input} type="number" name="subjectCategoryId" placeholder="Category ID" onChange={handleSubjectCategoryIdChange} />
+        <input ref={input => this.googlePlacesReference = input} type="text" name="googlePlacesReference" placeholder="Google Places Reference" onChange={handleGooglePlacesReferenceChange} />
+        <button type="button" value="Add subject" onClick={handleSubmit}>Add subject</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 const addSubjectMutation = gql`
   mutation addSubject (
@@ -51,6 +68,7 @@ const addSubjectMutation = gql`
         subjectCategoryId: $subjectCategoryId
         googlePlacesReference: $googlePlacesReference
       ) {
+        id
         subjectCategoryId
         googlePlacesReference
       }
