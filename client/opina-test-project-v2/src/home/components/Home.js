7a213340a1e1072395c458e04bca201412312bfd
FSVD <fabio.schettino.01@gmail.com>
2017-07-27 17:40:53 +0200

    Syntax updated

--- client/opina-test-project-v2/src/home/components/Home.js
+++ client/opina-test-project-v2/src/home/components/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const Home = ({ match }) => {
+// Functional component
+/* const Home = ({ match }) => {
   return (
     <h1>Welcome to Test project</h1>
   );
+};
+ */
+
+// Class Component
+class Home extends React.Component {
+
+  render() {
+    return (
+      <h1>Welcome to Test project</h1>
+    );
+  }
 };
 
 export default Home;
